863528b50f854a9af4f864045e661f9f22098d5c
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2019-12-23 08:26:13 +0530

    Initial Commit

---
+++ Project-Food-Order/src/main/java/com/saptrv/DBManager.java
@@ -0,0 +1,21 @@
+package com.saptrv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBManager {
+static Connection cn;
+public static java.sql.Connection getConnection()
+{
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/foodorder", "root", "manager");
+		
+	}catch(Exception ee)
+	{
+		ee.printStackTrace();
+	}
+	return cn;
+	
+}
+}
